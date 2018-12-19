165a695b0ba9d8ab450fc9ba055b4201a79f325b
Celestine <xfj2323005831@163.com>
2018-12-19 18:17:33 +0800

    微信小程序swiper设置中间大两边小banner

---
+++ pages/banner/banner.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    imgUrls: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545220114942&di=3bf52981626b3cc290dd684df29e714c&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F8b82b9014a90f6038de2556a3212b31bb051ed4a.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545220114942&di=3bf52981626b3cc290dd684df29e714c&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F8b82b9014a90f6038de2556a3212b31bb051ed4a.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1545220114942&di=3bf52981626b3cc290dd684df29e714c&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F8b82b9014a90f6038de2556a3212b31bb051ed4a.jpg'
+    ],
+    interval: 5000,
+    duration: 1000,
+    circular:true,
+    leftMargin:'80rpx',
+    rightMargin:'80rpx',
+    currentIndex:0
+  },
+  handleChange: function (e) {
+    this.setData({
+      currentIndex: e.detail.current
+    })
+  },
+})
